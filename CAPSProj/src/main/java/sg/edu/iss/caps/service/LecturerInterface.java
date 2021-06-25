eba01f708d04a3f6c5b55852a383f74d4fa9148b
Aye Phyu Sin <ayephyusin18994@gmail.com>
2021-06-25 09:32:01 +0800

    Revert "fix: courses.java and users.java"

--- CAPSProj/src/main/java/sg/edu/iss/caps/service/LecturerInterface.java
+++ CAPSProj/src/main/java/sg/edu/iss/caps/service/LecturerInterface.java
@@ -1,17 +1,5 @@
 package sg.edu.iss.caps.service;
 
-import java.util.List;
-
-import sg.edu.iss.caps.model.Courses;
-import sg.edu.iss.caps.model.Users;
-
 public interface LecturerInterface {
 
-	List<Courses> getAllCourses();
-	
-	Courses getCoursesById(Long id);
-	
-	List<Users> getAllUsers();
-	
-	Users getUsersById(Long id);
 }
