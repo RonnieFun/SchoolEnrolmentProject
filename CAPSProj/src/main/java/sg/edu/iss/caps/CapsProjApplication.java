05180b81b12d230666d9d6fb287ebb42029f63d1
Wenquan Chen <e0696865@u.nus.edu>
2021-06-25 21:56:23 +0800

    Testing

--- CAPSProj/src/main/java/sg/edu/iss/caps/CapsProjApplication.java
+++ CAPSProj/src/main/java/sg/edu/iss/caps/CapsProjApplication.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.mapping.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
