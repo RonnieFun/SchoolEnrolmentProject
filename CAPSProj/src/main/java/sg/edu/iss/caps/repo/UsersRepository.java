b18f5d16bb2ffb99c2b7fb9c87fd6f9e98b25dfd
Brandon <brandon.chia91@outlook.com>
2021-06-28 09:30:09 +0800

    Merged with main

--- CAPSProj/src/main/java/sg/edu/iss/caps/repo/UsersRepository.java
+++ CAPSProj/src/main/java/sg/edu/iss/caps/repo/UsersRepository.java
@@ -5,13 +5,20 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import sg.edu.iss.caps.model.Courses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import sg.edu.iss.caps.model.Roles;
 import sg.edu.iss.caps.model.Users;
+
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
 	
 	@Query("SELECT u FROM Users u WHERE u.role=:role")
 	List<Users> findByRole(@Param("role") Roles role);
+	
+	public Users findUserByfirstNameAndLastName(String firstName, String lastName);
+	
+	Page<Users> findByRole(Roles role, Pageable pageable);
 
 }
