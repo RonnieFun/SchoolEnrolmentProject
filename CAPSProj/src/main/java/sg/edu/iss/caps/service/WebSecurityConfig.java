6e4dab87267e64473e0ad0dd1484f622d48e2e86
Brandon <brandon.chia91@outlook.com>
2021-06-29 23:48:41 +0800

    minor bugs fix

--- CAPSProj/src/main/java/sg/edu/iss/caps/service/WebSecurityConfig.java
+++ CAPSProj/src/main/java/sg/edu/iss/caps/service/WebSecurityConfig.java
@@ -8,14 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+
+
 @Configuration
 @EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -49,8 +54,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/", "/home","/admin/list*/*").permitAll()
-			.antMatchers("/admin*").hasRole("ADMIN")
+			.antMatchers("/", "/home","/admin/list**").permitAll()
+			.antMatchers("/admin**").hasRole("ADMIN")
 			.anyRequest()
 			.permitAll()
 			//.authenticated()
