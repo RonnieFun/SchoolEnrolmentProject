f0765b9012cf46c418c9850cae9f1af5c9015285
Chen Wenquan <64681128+maxchen33@users.noreply.github.com>
2021-07-01 12:24:58 +0800

    Update CoursesRepository.java

--- CAPSProj/src/main/java/sg/edu/iss/caps/repo/CoursesRepository.java
+++ CAPSProj/src/main/java/sg/edu/iss/caps/repo/CoursesRepository.java
@@ -32,7 +32,7 @@ public interface CoursesRepository extends JpaRepository<Courses, Long> {
 	
 	Courses findByCourseNameAndCourseStartDate(String courseName, LocalDate courseStartDate);
 	
-	@Query("SELECT c FROM Courses c WHERE c.courseName = :courseName")
+	@Query("SELECT DISTINCT c FROM Courses c WHERE c.courseName = :courseName")
 	List<Courses> findCoursebyCourseName(@Param("courseName") String courseName);	
 
 	Courses getById(Long id);
