812925b71da9e76f15cf06da740450704159f6a7
Wenquan Chen <e0696865@u.nus.edu>
2021-07-03 00:08:27 +0800

    Updated layout.html page for lecturer module navbar links.
    Rearranged lecturerinterface to make it look neater

--- CAPSProj/src/main/java/sg/edu/iss/caps/service/LecturerInterface.java
+++ CAPSProj/src/main/java/sg/edu/iss/caps/service/LecturerInterface.java
@@ -11,27 +11,30 @@ import sg.edu.iss.caps.model.Users;
 
 public interface LecturerInterface {
 
-	//To get list of all courses
 	List<Courses> getAllCourses();
 	
-	//To get list of all courses by Course ID
 	List<Courses> findCoursesByCourseId(Long courseID);
+	
+	List<Courses> getAllCoursesByLecturerId(Long id);
 		
-	//To get list of all courses by Lecturer's Id
 	List<Courses> getAllCoursesByRoleAndId(Roles role, Long userID);
+	
+	List<Courses> findCoursebyCourseNameStartDateCourseID(String courseName, LocalDate courseStartDate);
 		
-	//To get all student grades by student id
 	List<Users> findUsersByRoleAndId(Long userID, Roles role);
 			
-	//To get list of all users
+	List<Users> getStudentsByCourseID(Long courseID);
+	
 	List<Users> getAllUsers();
 
-	//To get list of users by Role
 	List<Users> getAllUsersByRole(Roles role);
 	
 	List<Users> getAllUsersByUserID(Long userID);
 	
-	//To get list of users by Role, Course Name and Course Start
+	List<StudentCourseDetails> getAllStudentCourseDetails();
+	
+	List<StudentCourseDetails> getStudentCourseDetailsByCourseID(Long courseID);
+	
 	List<StudentCourseDetails> getAllUsersByRoleCourseNameStartDate(Roles role, EnrolmentStatus enrolmentStatus, String courseName, 
 			LocalDate courseStartDate);
 
@@ -43,20 +46,9 @@ public interface LecturerInterface {
 
 	void removeCourseTaught(Long id, Courses course);
 
-	List<Users> getStudentsByCourseID(Long courseID);
-
-	List<StudentCourseDetails> getStudentCourseDetailsByCourseID(Long courseID);
-
 	StudentCourseDetails getStudentCourseDetailsByStudentIDAndCourseID(long parseLong, long parseLong2);
 
 	void saveStudentCourseDetails(StudentCourseDetails studentCourseDetails);
 
-	List<Courses> getAllCoursesByLecturerId(Long id);
-
 	Courses getCoursesById(Long id);
-	
-	List<StudentCourseDetails> getAllStudentCourseDetails();
-	
-	List<Courses> findCoursebyCourseNameStartDateCourseID(String courseName, LocalDate courseStartDate);
-
 }
