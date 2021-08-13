e40cea6fbf477fb2df81a992fe083234bc486ead
RonnieFun <ronyfun@hotmail.com>
2021-08-13 14:15:29 +0800

    Updated to try deployment

--- CAPSProj/src/main/java/sg/edu/iss/caps/service/EmailSendingInterface.java
+++
@@ -1,9 +0,0 @@
-package sg.edu.iss.caps.service;
-
-import sg.edu.iss.caps.model.Courses;
-import sg.edu.iss.caps.model.Users;
-
-public interface EmailSendingInterface {
-	public void sendCourseEnrolmentConfirmation(Courses course, Users user);
-	public void sendAccountCreatedEmail(Users user, String unhashedpassword);
-}
